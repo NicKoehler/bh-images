767c12966abb04089287128acee0a7fabd9ffe79
NicKoehler <grillinicola@proton.me>
2024-11-05 09:04:54 +0100

    use api key

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from os import path, mkdir
 from requests import get
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+from os import path, mkdir, environ
 
 MINI_PATH = "mini"
 FULL_PATH = "full"
-URL = "https://brawlhalla.com"
+SITE_URL = "https://brawlhalla.com"
+API_URL = "https://api.brawlhalla.com"
+API_KEY = None
 
 
 def scrape_full_image(full_path, link):
@@ -24,7 +27,13 @@ def scrape_full_image(full_path, link):
 
 
 if __name__ == "__main__":
-    bs = BeautifulSoup(get(f"{URL}/legends").content.decode("utf-8"), "html.parser")
+
+    load_dotenv()
+
+    if "API_KEY" in environ:
+        API_KEY = environ["API_KEY"]
+    else:
+        raise RuntimeError("API_KEY is not set")
 
     if not path.exists(MINI_PATH):
         mkdir(MINI_PATH)
@@ -32,14 +41,26 @@ if __name__ == "__main__":
     if not path.exists(FULL_PATH):
         mkdir(FULL_PATH)
 
-    for tag in filter(
-        lambda a: "href" in a.attrs and a.attrs["href"].startswith("/legends/"),
-        bs.find_all("a"),
+    legends = get(f"{API_URL}/legend/all", params={"api_key": API_KEY}).json()
+
+    bs = BeautifulSoup(
+        get(f"{SITE_URL}/legends").content.decode("utf-8"), "html.parser"
+    )
+
+    for legend, tag in zip(
+        legends,
+        filter(
+            lambda a: "href" in a.attrs and a.attrs["href"].startswith("/legends/"),
+            bs.find_all("a"),
+        ),
     ):
         image_link = tag.find("img").attrs["src"]
-        filename = f"{tag.find('h3').text.lower()}.png".replace("ö", "o")
+        filename = f"{legend.get('legend_name_key')}.png"
         full_path = path.join(MINI_PATH, filename)
-        scrape_full_image(path.join(FULL_PATH, filename), f"{URL}{tag.attrs['href']}")
+
+        scrape_full_image(
+            path.join(FULL_PATH, filename), f"{SITE_URL}{tag.attrs['href']}"
+        )
 
         if path.exists(full_path):
             continue
